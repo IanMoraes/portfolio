00d0fab8564f5543dd28b33c01659ff0c630c4e7
IanMoraes <ianmoraes1@gmail.com>
2023-12-07 23:12:37 -0300

    fix first stage

--- app/page.tsx
+++ app/page.tsx
@@ -17,21 +17,22 @@ export default function Home() {
   return (
     <section className="flex  items-center justify-center  ">
       <Card className="w-full  bg-gray-400 backdrop-filter backdrop-blur-sm bg-opacity-10 ">
-        <CardBody className="flex flex-row gap-8">
+        <CardBody className="flex  flex-col   gap-8 md:flex-row justify-center items-center backdrop-blur-none">
           <Image
             isZoomed
             as={NextImage}
-            width={240}
-            height={240}
+            width={300}
+            height={300}
             alt="NextUI Fruit Image with Zoom"
             src="/photo.jpg"
+			className="w-72"
           />
-          <div>
+          <div className="md:text-left text-center ">
             <h1 className="text-4xl font-bold">{siteConfig.name}</h1>
             <h2 className="text-2xl">{siteConfig.description}</h2>
             <br />
             <p className="text-xl">
-              Desenvolvedor com {yearsOfExperience} anos de experiência em desenvolvimento, manutenção de sites e aplicativos
+              Desenvolvedor com {yearsOfExperience} anos de experiência em desenvolvimento e manutenção de sites e aplicativos de alta qualidade
             </p>
             
           </div>
--- components/navbar.tsx
+++ components/navbar.tsx
@@ -51,10 +51,10 @@ export const Navbar = () => {
         </NavbarContent>
 
         <NavbarContent
-          className="hidden sm:flex basis-1/5 sm:basis-full"
+          className="flex basis-1/5 sm:basis-full"
           justify="end"
         >
-          <NavbarItem className="hidden sm:flex gap-2">
+          <NavbarItem className="flex gap-2">
             <Link
               isExternal
               href={siteConfig.links.linkedin}
